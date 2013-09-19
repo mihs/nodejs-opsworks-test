5f4bae03cb8498192c0b11db8ee889fdf0fd448a
Mihnea <mihnea@nagemus.com>
2013-09-19 17:16:33 +0300

    initial commit

---
+++ server2.js
@@ -0,0 +1,12 @@
+var express = require("express"),
+  http = require("http");
+
+var app = express();
+var port = 8080;
+
+app.get("/", function(req, res){
+  res.send("HELLO WORLD 2");
+});
+server = http.createServer(app);
+server.listen(port);
+console.log("Started on port", port);
