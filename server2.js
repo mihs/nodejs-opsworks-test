e671e93765f135e6e524d19ec1d142f016e9611c
Mihnea <mihnea@nagemus.com>
2013-10-18 14:39:16 +0300

    write environment during npm to a file

--- server2.js
+++ server2.js
@@ -1,11 +1,19 @@
 var express = require("express"),
-  http = require("http");
+  http = require("http"),
+  fs = require("fs");
 
 var app = express();
-var port = 80;
+var port = process.env.PORT || 80;
 
 app.get("/", function(req, res){
-  res.send("HELLO WORLD 2");
+  res.send("HELLO WORLD 2 - env: " + JSON.stringify(process.env));
+});
+app.get("/npmenv", function(req, res){
+  fs.readFile('./npmenv', function(err, data){
+    if (err) throw err;
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(data);
+  })
 });
 server = http.createServer(app);
 server.listen(port);
